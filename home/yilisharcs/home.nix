a3fc35e7ff9a7077a5b94d40ff16938fb4764502
yilisharcs <yilisharcs@gmail.com>
2025-05-16 10:22:30 -0300

    editorconfig

--- home/yilisharcs/home.nix
+++ home/yilisharcs/home.nix
@@ -166,6 +166,18 @@
     };
   };
 
+  editorconfig = {
+    enable = true;
+    settings = {
+      "*" = {
+        charset = "utf-8";
+        end_of_line = "lf";
+        insert_final_newline = true;
+        trim_trailing_whitespace = true;
+      };
+    };
+  };
+
   services = {
     # gnome = {
     #   gnome-keyring = {
