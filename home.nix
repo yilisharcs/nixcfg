ef27527eb9e5c57af4b5ef50402fc7e9ea6724e2
yilisharcs <yilisharcs@gmail.com>
2025-05-24 05:52:32 -0300

    disable syncthing autoaccept

--- home.nix
+++ home.nix
@@ -1023,7 +1023,7 @@
       devices = {
         "samsumg" = {
           id = "MF6IH63-BX3WP45-ZH42ZJM-ZKA45EZ-JNDUF5L-ZDLO5KH-H5B6XRJ-O4CTEQW";
-          autoAcceptFolders = true;
+          # autoAcceptFolders = true; #NOTE: This setting causes duplication
         };
       };
       folders = {
