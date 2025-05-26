29c6282476d5b327a786fd0f0e0e835fa14c37c1
yilisharcs <yilisharcs@gmail.com>
2025-05-26 14:04:57 -0300

    batteries included yazi replaces lf (thanks noboilerplate)

--- home.nix
+++ home.nix
@@ -142,7 +142,6 @@
     #   echo "Hello, ${config.home.username}!"
     # '')
 
-    ctpv                           # lf previewer
     dconf-editor
     entr                           # file watcher
     # evolution                      # mail client
@@ -159,7 +158,6 @@
     imagemagick
     inkscape                       # image editor
     # libreoffice-qt                 # office suite
-    lf                             # tui file explorer
     # man-pages                      # Linux man pages
     # man                            # man command
     mesa                           # graphics lib
@@ -179,9 +177,9 @@
     xclip
 
     # compression and extraction tools
-    atool
-    unzip
-    zip
+    ouch
+    unrar
+    zstd
 
     # LSP
     lua-language-server
@@ -521,12 +519,6 @@
       hidden = true;
     };
 
-    # lf = {
-    #   enable = true;
-    #   ## FIXME: previewer doesn't work
-    #   # previewer = "${pkgs.ctpv}/bin/bash";
-    # };
-
     kitty = {
       enable = true;
       package = (config.lib.nixGL.wrap pkgs.kitty);
@@ -754,6 +746,122 @@
         bind -T copy-mode-vi 'v' send -X begin-selection
         bind -T copy-mode-vi 'y' send -X copy-pipe-and-cancel 'xclip -sel clip'
       '';
+    };
+
+    yazi = {
+      enable = true;
+      shellWrapperName = "yf";
+      plugins  = {
+        ouch = pkgs.yaziPlugins.ouch;
+      };
+      theme = {
+        mode = {
+          normal_main = { bg = "blue"; fg = "black"; bold = true; };
+          normal_alt  = { fg = "blue"; bg = "black"; };
+          select_main = { bg = "red";  fg = "black"; bold = true; };
+          select_alt  = { fg = "red";  bg = "black"; };
+          unset_main  = { bg = "red";  fg = "black"; bold = true; };
+          unset_alt   = { fg = "red";  bg = "black"; };
+        };
+      };
+      settings = {
+        plugin = {
+          prepend_previewers = [
+            { mime = "application/gzip";  run = "ouch"; }
+            { mime = "application/x-xz";  run = "ouch"; }
+            { mime = "application/zip";   run = "ouch"; }
+            { mime = "application/zstd";  run = "ouch"; }
+          ];
+        };
+        manager = {
+          sort_by = "natural";
+          sort_sensitive = false;
+          sort_dir_first = true;
+          sort_translit = true;
+          linemode = "mtime";
+          show_hidden = true;
+          show_symlink = true;
+          scrolloff = 2;
+        };
+        preview = { wrap = "yes"; };
+        opener = {
+          play = [
+            {
+              desc = "Open with GNOME video player";
+              run = ''totem "$@"'';
+              orphan = true;
+              for = "unix";
+            }
+          ];
+        };
+        open = {
+          prepend_rules = [
+            { mime = "text/*";            use = "edit"; }
+            { mime = "inode/x-empty";     use = "edit"; }
+            { name = "*.html";            use = ["open" "edit"]; }
+            { mime = "application/gzip";  use = "edit"; }
+            { mime = "application/x-xz";  use = "edit"; }
+            { mime = "application/zip";   use = "edit"; }
+            { mime = "application/zstd";  use = "edit"; }
+          ];
+        };
+        confirm = {
+          trash = true;
+          delete = true;
+          overwrite = true;
+        };
+      };
+      keymap = {
+        manager.prepend_keymap = [
+          # <C-m> is <Enter>, <C-i> is <Tab>
+          { on = "<C-m>"; run = "open"; }
+          { on = "<C-i>"; run = "spot"; desc = "Spot hovered file"; }
+          { on = "<C-g>"; run = "shell 'nu' --block"; }
+
+          # Compress and extract
+          { on = ["e" "e"]; run = "shell 'ouch d \"$@\"'"; desc = "Extract archive"; }
+          {
+            on = ["e" "p"];
+            run = "shell 'ouch c -S \"$@\" .tar.zst' --interactive --cursor=15 --orphan";
+            desc = "Compress as tar.zst";
+          }
+
+          # Backup
+          { on = "b"; run = "shell 'cp $0 $0.bak'"; desc = "Create file backup"; }
+
+          # Pager
+          { on = "i"; run = "shell 'bat $0' --block"; }
+
+          # Restore trashed files
+          {
+            on = "u";
+            run = ''shell 'file=$(trash-list | grep "$(pwd)" | fzf --preview-window hidden | cut -b21-) \
+              && [ -n "$file" ] && yes 0 | trash-restore "$file"' --block'';
+            desc = "Restore trashed file";
+          }
+
+          # Goto
+          { on = ["g" "b"]; run = "cd ~/notebook";                     desc = "Go ~/notebook"; }
+          { on = ["g" "B"]; run = "cd ~/vault";                        desc = "Go ~/vault"; }
+          { on = ["g" "c"]; run = "cd ~/.config";                      desc = "Go ~/.config"; }
+          { on = ["g" "d"]; run = "cd ~/Downloads";                    desc = "Go ~/Downloads"; }
+          { on = ["g" "f"]; run = "follow";                            desc = "Follow hovered symlink"; }
+          { on = ["g" "i"]; run = "cd ~/Pictures";                     desc = "Go ~/Pictures"; }
+          { on = ["g" "l"]; run = "cd ~/.dotfiles";                    desc = "Go ~/.dotfiles"; }
+          { on = ["g" "m"]; run = "cd ~/Music";                        desc = "Go ~/Music"; }
+          { on = ["g" "n"]; run = "cd ~/.dotfiles/nvim/.config/nvim";  desc = "Go to nvim/init.lua"; }
+          { on = ["g" "p"]; run = "cd ~/projects";                     desc = "Go ~/projects"; }
+          { on = ["g" "r"]; run = "cd ~/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f"; desc = "Go to cargo registry"; }
+          { on = ["g" "s"]; run = "cd ~/.dotfiles/bin/.local/bin";     desc = "Go to private /bin"; }
+          { on = ["g" "v"]; run = "cd ~/Videos";                       desc = "Go ~/Videos"; }
+          { on = ["g" "z"]; run = "cd ~/.local/share/nvim/lazy";       desc = "Go to lazydir"; }
+        ];
+
+        # TODO Make `C-m` `C-h` `C-c` and `C-[` work in the Help panel
+        pick.prepend_keymap   = [{ on = "<C-m>"; run = "close --submit"; }];
+        input.prepend_keymap  = [{ on = "<C-m>"; run = "close --submit"; }];
+        cmp.prepend_keymap    = [{ on = "<C-m>"; run = "close --submit"; }];
+      };
     };
 
     # YouTube downloader
@@ -1043,7 +1151,7 @@
       genericName = "Text Editor";
       comment = "Edit text files";
       icon = "nvim";
-      exec = ''kitty --start-as fullscreen nvim "%F"'';
+      exec = ''kitty --start-as fullscreen nvim'';
       terminal = false;
       startupNotify = false;
       mimeType = [
