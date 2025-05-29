b941450724bd145afb3aaca3919ab5818be3ffa5
yilisharcs <yilisharcs@gmail.com>
2025-05-29 11:45:01 -0300

    yazi shell block doesn't seem to support aliases so it's pointless

--- home.nix
+++ home.nix
@@ -871,15 +871,11 @@
         };
       };
       keymap = {
-        tasks.prepend_keymap = [
-          { on = "W"; run = "close"; desc = "Close task manager"; }
-        ];
         manager.prepend_keymap = [
           # <C-m> is <Enter>, <C-i> is <Tab>
           { on = "<C-m>"; run = "open"; }
           { on = "<C-i>"; run = "spot"; desc = "Spot hovered file"; }
-          { on = "w"; run = "shell 'nu' --block"; desc = "Launch a shell"; }
-          { on = "W"; run = "tasks:show"; desc = "Show task manager"; }
+          { on = ":"; run = "shell 'nu' --block"; desc = "Launch a shell"; }
 
           # Smart enter
           { on = "l"; run  = "plugin smart-enter"; desc = "Enter the child directory, or open the file"; }
