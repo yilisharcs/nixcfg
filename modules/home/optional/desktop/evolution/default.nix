d73a599cd14560b89b48542d96a9d35f1f902666
yilisharcs <yilisharcs@gmail.com>
2025-06-15 12:04:45 -0300

    manage evolution with home manager

---
+++ modules/home/optional/desktop/evolution/default.nix
@@ -0,0 +1,35 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  home.packages = [
+    pkgs.evolution
+  ];
+
+  dconf.settings = {
+    "org/gnome/evolution/calendar" = {
+      use-24hour-format = true;
+    };
+    "org/gnome/evolution/mail" = {
+      composer-magic-smileys = true;
+      composer-unicode-smileys = true;
+      image-loading-policy = "always";
+      layout = 1;
+      message-list-sort-on-header-click = "always";
+      show-animated-images = true;
+      show-to-do-bar = false;
+    };
+    "org/gnome/evolution/plugin/external-editor" = {
+      # command = "neovide -- -c 'set spell' -c 'startinsert'";
+      command = "kitty nvim -c 'set spell' -c 'startinsert'";
+      launch-on-key-press = true;
+    };
+    "org/gnome/evolution/shell" = {
+      icon-only-buttons-in-header-bar = true;
+      statusbar-visible = false;
+      webkit-minimum-font-size = 16;
+    };
+  };
+}
--- modules/system/optional/pkgs/desktop/mail/evolution.nix
+++
@@ -1,9 +0,0 @@
-{
-  programs.evolution = {
-    enable = true;
-  };
-
-  # TODO: add dconf entries
-  # consider managing this through home manager
-
-}
