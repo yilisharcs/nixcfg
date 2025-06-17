8dfbe472028a663fb9f8ba831f817cf787a9d270
yilisharcs <yilisharcs@gmail.com>
2025-06-17 10:06:43 -0300

    hyprnot

--- modules/system/optional/environments/hyprland.nix
+++ modules/system/optional/environments/hyprland.nix
@@ -1,4 +1,9 @@
 {
+  pkgs,
+  ...
+}:
+
+{
   imports = [
     ./common
   ];
@@ -6,5 +11,13 @@
   # TODO: THIS IS INCOMPLETE
 
   # NOTE: home.pointerCursor has a hyprcursor option
-
+  # DOESN'T BELONG HERE
+  # home.pointerCursor = {
+  #   enable = true;
+  #   gtk.enable = true;
+  #   # hyprcursor = { enable = true; size = 24; };
+  #   package = pkgs.bibata-cursors;
+  #   name = "Bibata-Modern-Ice";
+  #   size = 24;
+  # };
 }
