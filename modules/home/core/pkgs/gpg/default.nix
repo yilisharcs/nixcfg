466489910b6bd7956157d2f0fac5c5223aa4474f
yilisharcs <yilisharcs@gmail.com>
2025-06-21 12:31:13 -0300

    add pinentry package for gnupg

--- modules/home/core/pkgs/gpg/default.nix
+++ modules/home/core/pkgs/gpg/default.nix
@@ -1,7 +1,21 @@
 {
+  pkgs,
+  ...
+}:
+
+{
   programs.gpg = {
     enable = true;
     mutableKeys = true;
     mutableTrust = true;
+    settings = {
+      keyid-format = "none";
+    };
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    # enableSshSupport = true;
+    pinentry.package = pkgs.pinentry-tty;
   };
 }
