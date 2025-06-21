5007dceaad3c511945136fbd0d4ea34309801a65
yilisharcs <yilisharcs@gmail.com>
2025-06-21 16:44:21 -0300

    nvim file templates

--- modules/users/yilisharcs/nvim/config/plugin/placeholders.lua
+++ modules/users/yilisharcs/nvim/config/plugin/placeholders.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("i", ",gnu", "<ESC>ggdG:0read ~/.config/nvim/skeletons/license/gpl-3.0.txt | $d_<CR>gg")
-vim.keymap.set("i", ",lorem", "<ESC>ggdG:0read ~/.config/nvim/skeletons/lorem/ipsum.txt | $d_<CR>gg")
+vim.keymap.set("i", ",lorem", "<ESC>:read ~/.config/nvim/skeletons/lorem/ipsum.txt<CR>o")
+vim.keymap.set("i", ",nix", "<ESC>ggdG:0read ~/.config/nvim/skeletons/flakes/default.nix | $d_<CR>O")
